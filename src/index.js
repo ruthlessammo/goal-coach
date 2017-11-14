adf3e68cb2f6e866d8bfd5037c563d4963e899a8
ruthlessammo <williamjchilton@gmail.com>
2017-11-14 13:16:01 +0000

    added primary components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <div>App</div>, document.getElementById('root')
+)
