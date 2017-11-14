b06c84de84d398e62f59eae1a937702ee71cfde3
ruthlessammo <williamjchilton@gmail.com>
2017-11-14 19:31:58 +0000

    props and state comong thru in addGoal now need to make the goals compnt

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { firebaseApp } from '../firebase';
 import { connect } from 'react-redux'
-
+import AddGoal from './AddGoal'
 class App extends Component {
   signOut() {
     firebaseApp.auth().signOut();
@@ -10,7 +10,9 @@ class App extends Component {
   render() {
     return (
       <div>
-      App
+        <h3>Goals</h3>
+        <AddGoal />
+        <div>Goal List</div>
         <button
           className="btn btn-danger"
           onClick={() => this.signOut()}
--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,13 @@
 import * as firebase from 'firebase'
 
 const config = {
-    apiKey: "AIzaSyDmPvIdbUprPIE23fHrTXqeSssT2Pb6tA0",
-    authDomain: "goal-coach-ffcbd.firebaseapp.com",
-    databaseURL: "https://goal-coach-ffcbd.firebaseio.com",
-    projectId: "goal-coach-ffcbd",
-    storageBucket: "",
-    messagingSenderId: "311828817347"
-  };
+  apiKey: "AIzaSyDmPvIdbUprPIE23fHrTXqeSssT2Pb6tA0",
+  authDomain: "goal-coach-ffcbd.firebaseapp.com",
+  databaseURL: "https://goal-coach-ffcbd.firebaseio.com",
+  projectId: "goal-coach-ffcbd",
+  storageBucket: "",
+  messagingSenderId: "311828817347"
+};
 
-  export const firebaseApp = firebase.initializeApp(config);
+export const firebaseApp = firebase.initializeApp(config);
+export const goalRef = firebase.database().ref('goals');
