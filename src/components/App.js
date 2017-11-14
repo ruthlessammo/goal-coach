6baa4d456397898da94a4cbec6a208b90850facc
ruthlessammo <williamjchilton@gmail.com>
2017-11-14 17:58:22 +0000

    logged user with redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { firebaseApp } from '../firebase';
+import { connect } from 'react-redux'
 
 class App extends Component {
   signOut() {
     firebaseApp.auth().signOut();
   }
-  
+
   render() {
     return (
       <div>
@@ -19,4 +20,9 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  console.log(state);
+  return {}
+}
+
+export default connect(mapStateToProps, null)(App);
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -15,7 +15,7 @@ class SignIn extends Component {
   }
 
   signIn() {
-    console.log('this.state', this.state);
+    // console.log('this.state', this.state);
     const { email, password } = this.state;
     firebaseApp.auth().signInWithEmailAndPassword(email, password)
       .catch(error => {
